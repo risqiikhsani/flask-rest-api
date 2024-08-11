e8430253a314da008a75afe005fab7a710124a31
risqiikhsani <risqiikhsani16@gmail.com>
2024-08-11 15:33:37 +0700

    tst

---
+++ api.py
@@ -0,0 +1,83 @@
+from flask import Flask,request,abort
+
+from flask_restful import Resource,Api,fields,marshal_with,reqparse
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+api = Api(app)
+db = SQLAlchemy(app)
+
+
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User(name = {self.name}, email={self.email})"
+    
+user_args = reqparse.RequestParser()
+user_args.add_argument('name',type=str,required=True,help="Name cannot be blank")
+user_args.add_argument('email',type=str,required=True,help="Email cannot be blank")
+
+userFields = {
+    'id':fields.Integer,
+    'name':fields.String,
+    'email':fields.String
+}
+
+class Users(Resource):
+    @marshal_with(userFields)
+    def get(self):
+        users = UserModel.query.all()
+        return users
+
+    @marshal_with(userFields)    
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(name=args["name"],email=args["email"])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users, 201
+    
+class User(Resource):
+    @marshal_with(userFields)
+    def get(self, id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,message="User not found")
+        return user
+    
+    @marshal_with(userFields)
+    def put(self, id):
+        args = user_args.parse_args()
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,message="User not found")
+        user.name = args["name"]
+        user.email= args["email"]
+        db.session.commit()
+        return user
+    
+    @marshal_with(userFields)
+    def delete(self, id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,message="User not found")
+        db.session.delete(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users
+
+api.add_resource(Users,'/api/users')
+api.add_resource(User,'/api/user/<int:id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
